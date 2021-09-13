5ac6a15f27e9ad7c1558efeb0a066abb2e0d763a
Andrew <nikamoda@gmail.com>
2021-09-13 18:48:06 +1000

    until u13

--- unit_10/unit_10.js
+++ unit_10/unit_10.js
@@ -1,10 +1,10 @@
 // Task 1
 //Создайте массив ar1 содержащий строки, числа, булевы значения. Выведите его в .out-1. Вывод - по нажатию кнопки b-1
 
-// let ar1 = // переменную обьявляем здесь!!!!
+let ar1 = [1,3,-8, 'Andrew', true, 888];
 
 function f1() {
-    //ваш_элемент.innerHTML = ar1; // да так можно выводить!!!!
+    document.querySelector('.out-1').innerHTML = ar1; // да так можно выводить!!!!
 }
 
 document.querySelector('.b-1').onclick = f1;
@@ -12,14 +12,14 @@ document.querySelector('.b-1').onclick = f1;
 // Task 2
 //Создайте массив ar2 содержащий строки, числа, булевы значения. Выведите его в div.out-2. Используйте шаблон вывода из кода в JS. Вывод - по нажатию кнопки b-2
 
-// let ar2 = // переменную обьявляем здесь!!!!
+let ar2 = ['value', 600, false, 'motorcycle']; // переменную обьявляем здесь!!!!
 
 function f2() {
-    // let out = '';
-    // for (let i = 0; i < ar2.length; i++) {
-    //     out +=arr2[i]+' ';
-    // }
-    // document.querySelector('.out-2').innerHTML = out;
+    let out = '';
+    for (let i = 0; i < ar2.length; i++) {
+        out +=ar2[i]+' ';
+    }
+    document.querySelector('.out-2').innerHTML = out;
 }
 
 document.querySelector('.b-2').onclick = f2;
@@ -30,9 +30,10 @@ document.querySelector('.b-2').onclick = f2;
 // Вывод в out-3
 // Тест допустим массив [1,2,3] вывод - 3
 
-// let ar3 =  // переменную обьявляем здесь!!!!
+let ar3 = [1,56,2,7,87,3, true]; // переменную обьявляем здесь!!!!
 
 function f3() {
+    document.querySelector('.out-3').innerHTML = ar3.length;
 }
 
 document.querySelector('.b-3').onclick = f3;
@@ -44,9 +45,12 @@ document.querySelector('.b-3').onclick = f3;
 // Вывод в out-4
 // Тест допустим массив [1,2,3,4,5,6,7,8,9,10] вывод: 1 4 9
 
-// let ar4 =  // переменную обьявляем эту здесь!!!!
+let ar4 = [1,2,3,4,5,6,7,8,9,10]; // переменную обьявляем эту здесь!!!!
 
 function f4() {
+    // document.querySelector('.out-4').innerHTML = ar4[0] + ' ' + ar4[3] + ' ' + ar4[8];
+    document.querySelector('.out-4').innerHTML = `${ar4[0]} ${ar4[3]} ${ar4[8]}`;
+
 }
 
 document.querySelector('.b-4').onclick = f4;
@@ -58,9 +62,10 @@ document.querySelector('.b-4').onclick = f4;
 // Вывод в out-5
 // Тест допустим массив [1,2,3,4,5,6,7,8,9,10] вывод: 8
 
-// let ar5 =
+let ar5 =[1,2,3,4,5,6,7,8,9,10];
 
 function f5() {
+    document.querySelector('.out-5').innerHTML = ar5[0] + ar5[2] + ar5[3];
 }
 
 document.querySelector('.b-5').onclick = f5;
@@ -71,9 +76,14 @@ document.querySelector('.b-5').onclick = f5;
 // Вывод - по нажатию кнопки b-6
 // Вывод в out-6
 
-// let ar6 =
+let ar6 = ['Andrew', 'Libra', 19, 'October'];
 
 function f6() {
+    let out = '';
+    for (let i = 0; i < ar6.length; i++) {
+        out +=ar6[i]+' ';
+    }
+    document.querySelector('.out-6').innerHTML = out;
 }
 
 document.querySelector('.b-6').onclick = f6;
@@ -87,6 +97,15 @@ document.querySelector('.b-6').onclick = f6;
 let ar7 = ['china', 'india', 'brazil', 'japan', 'egypt'];
 
 function f7() {
+    let out = '';
+    ar7[5] = 'italy';
+    ar7[6] = 'turkey';
+    ar7[7] = 'vietnam';
+
+    for(let i = 0; i < ar7.length; i++) {
+        out += ar7[i] + ' ';
+    }
+    document.querySelector('.out-7').innerHTML = out;
 }
 
 document.querySelector('.b-7').onclick = f7;
@@ -101,6 +120,17 @@ document.querySelector('.b-7').onclick = f7;
 let ar8 = [];
 
 function f8() {
+    ar8[3] = 3.14;
+    ar8[4] = 17;
+    ar8[6] = 5;
+
+    let out = '';
+    for (let i = 0; i < ar8.length; i++) {
+        out +=ar8[i]+'-';
+    }
+
+    document.querySelector('.out-8').innerHTML = out;
+    document.querySelector('.out-8-1').innerHTML = ar8.length;
 }
 
 document.querySelector('.b-8').onclick = f8;
@@ -114,6 +144,7 @@ document.querySelector('.b-8').onclick = f8;
 let ar9 = [100, 200, 300, 400, 700, 121];
 
 function f9() {
+    document.querySelector('.out-9').innerHTML = ar9[ar9.length - 1];
 }
 
 document.querySelector('.b-9').onclick = f9;
@@ -127,6 +158,7 @@ document.querySelector('.b-9').onclick = f9;
 let ar10 = [100, 200, 300, 400, 700, 121];
 
 function f10() {
+    document.querySelector('.out-10').innerHTML = ar10[1] + ar10[ar10.length-1];
 }
 
 document.querySelector('.b-10').onclick = f10;
@@ -142,6 +174,18 @@ document.querySelector('.b-10').onclick = f10;
 let ar11 = [2, 3, 4, 5, 6, 7];
 
 function f11() {
+ let t;
+ let out = '';
+
+ t = ar11[2];
+ ar11[2] = ar11[4];
+ ar11[4] = t;
+
+ for (let i = 0; i < ar11.length; i++) {
+    out +=ar11[i]+' ';
+}
+
+ document.querySelector('.out-11').innerHTML = out;
 
 }
 
@@ -157,7 +201,18 @@ document.querySelector('.b-11').onclick = f11;
 let ar12 = ['test', 'west', 'list', 'class', 'best'];
 
 function f12() {
-
+    let t;
+    let out = '';
+   
+    t = ar12[0];
+    ar12[0] = ar12[ar12.length - 1];
+    ar12[ar12.length - 1] = t;
+   
+    for (let i = 0; i < ar12.length; i++) {
+       out +=ar12[i]+' ';
+   }
+   
+    document.querySelector('.out-12').innerHTML = out;
 }
 
 document.querySelector('.b-12').onclick = f12;
@@ -174,7 +229,12 @@ document.querySelector('.b-12').onclick = f12;
 let ar13 = ['test', 'west', 'list', 'class', 'best'];
 
 function f13() {
+    let out = '';
 
+    for (let i = 0; i < ar13.length; i++) {
+        out += i + ' ' + ar13[i] + ' ';
+    }
+    document.querySelector('.out-13').innerHTML = out;
 }
 
 document.querySelector('.b-13').onclick = f13;
@@ -189,7 +249,12 @@ document.querySelector('.b-13').onclick = f13;
 let ar14 = [1, 2, 3, 'hello', 66];
 
 function f14() {
+    let out = '';
 
+    for(i = (ar14.length - 1); i >= 0; i--) {
+        out += ar14[i] + ' ';
+    }
+    document.querySelector('.out-14').innerHTML = out;
 }
 
 document.querySelector('.b-14').onclick = f14;
@@ -203,7 +268,14 @@ document.querySelector('.b-14').onclick = f14;
 let ar15 = [0, 2, 5, -4, 6, 22, -9, -12, 8, 12, 13, 78];
 
 function f15() {
+    let out = '';
 
+    for (i = 0; i < ar15.length; i++) {
+        if (ar15[i] > 0) {
+            out += ar15[i] + ' ';
+        }
+    }
+    document.querySelector('.out-15').innerHTML = out;
 }
 
 document.querySelector('.b-15').onclick = f15;
@@ -219,7 +291,22 @@ let ar16_odd = [];
 let ar16_even = [];
 
 function f16() {
+    let out_even = '';
+    let out_odd = '';
 
+    for (let i = 0; i < ar16.length; i++) {
+        if (ar16[i] % 2 === 0) {
+            ar16_even.push(ar16[i]);
+            out_even += ar16[i] + ' ';
+        }
+        else {
+            ar16_odd.push(ar16[i]);
+            out_odd += ar16[i] + ' ';
+        }
+    }
+
+    document.querySelector('.out-16-odd').innerHTML = out_odd;
+    document.querySelector('.out-16-even').innerHTML = out_even;
 }
 
 document.querySelector('.b-16').onclick = f16;
@@ -233,7 +320,14 @@ document.querySelector('.b-16').onclick = f16;
 let ar17 = [3, 0, 2, 6, 0, 1, 3, 1, 9, 0, 2, 0];
 
 function f17() {
+    let out = [];
 
+    for (let i = 0; i < ar17.length; i++) {
+        if (ar17[i] > 3) {
+            out += ar17[i];
+        }
+    }
+    document.querySelector('.out-17').innerHTML = out.length;
 }
 
 document.querySelector('.b-17').onclick = f17;
@@ -248,7 +342,15 @@ document.querySelector('.b-17').onclick = f17;
 let ar18 = [15, 24, 13, 78, 21, 4, 45, 67];
 
 function f18() {
+    let max = ar18[0];
 
+    for (let i = 0; i < ar18.length; i++) {
+        if ( ar18[i] > max) {
+            max = ar18[i];
+        }
+        document.querySelector('.out-18').innerHTML = max;
+    }
+    // alternative: Math.max(...ar18)
 }
 
 document.querySelector('.b-18').onclick = f18;
@@ -263,7 +365,14 @@ document.querySelector('.b-18').onclick = f18;
 let ar19 = [15, 424, 313, 78, 241, 4, 45, 67];
 
 function f19() {
+    let min = ar19[0];
 
+    for (let i = 0; i < ar18.length; i++) {
+        if ( ar19[i] < min) {
+            min = ar19[i];
+        }
+        document.querySelector('.out-19').innerHTML = min;
+    }
 }
 
 document.querySelector('.b-19').onclick = f19;
@@ -277,7 +386,12 @@ document.querySelector('.b-19').onclick = f19;
 let ar20 = [4, 5, 6, 7, 8, 9, 10];
 
 function f20() {
-
+    let sum = 0;
+    for (let i = 0; i < ar20.length; i++) {
+        sum += ar20[i];
+    }
+    document.querySelector('.out-20').innerHTML = sum;
+    // alternative: let sum = (acc,i) => acc + i;
 }
 
 document.querySelector('.b-20').onclick = f20;
